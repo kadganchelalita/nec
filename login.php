01d32236bdd9a31b9fbe11483f5943d3a0207780
kadganchelalita <156527718+kadganchelalita@users.noreply.github.com>
2024-01-30 17:37:00 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,19 +3,18 @@ session_start();
 
 include_once('config/db.php');
 include_once('includes/functions.php');
-
 if (isLoggedIn()) {
     redirectTo('dashboard.php');
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = sanitizeInput($_POST['username']);
-    $password = $_POST['password'];
-
+    $username = sanitizeInput($_POST['uname']);
+    $password = $_POST['psw'];
+   
     $stmt = $db->prepare("SELECT * FROM users WHERE username = ?");
     $stmt->execute([$username]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
+    
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
         redirectTo('dashboard.php');
